35bda87185a378d52c07df79fa499e02aaeea41f
Pradeep Kumar Yadav <60665024+pradeepkumarjse@users.noreply.github.com>
2024-07-23 21:03:52 +0530

    Update query_to_find_source_ip.sql

--- aws/cloudtrail/query_to_find_source_ip.sql
+++ aws/cloudtrail/query_to_find_source_ip.sql
@@ -6,7 +6,7 @@ FROM
     WHERE eventSource = 's3.amazonaws.com' AND 	
 userIdentity.accountid = '' 
  AND element_at(requestParameters, 'bucketName') IS NOT NULL
-    AND element_at(requestParameters, 'bucketName') IN ('slodispatch', 'slosales', 'sloimpoil', 'sloimpmn', 'sloimplpo', 'sloimpimsque', 'sloimpalm', 'sloimpac') AND
+    AND element_at(requestParameters, 'bucketName') IN ('', '', '', '', '', '', '', '') AND
     eventName IS NOT NULL AND eventName IN ('PutObject','GetObject')
     AND
 eventTime > '2024-04-19 22:00:22.000';
