6568e9201f09fc044caa6e125d318aa41325fefa
Pradeep Kumar Yadav <60665024+pradeepkumarjse@users.noreply.github.com>
2024-09-10 23:58:40 +0530

    Update backup_to_s3.sh

--- wordpress/backup_to_s3.sh
+++ wordpress/backup_to_s3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%F")
 BACKUP_DIR="/var/www/backup"
-WP_PATH="/var/www/html/your-wordpress-folder"
+WP_PATH="/var/www/vhosts/pradeeptech.info"
 DB_USER="main_admin"
-DB_PASSWORD="Kishor@1234"   # Add your DB password here
 DB_NAME="main_wordpress"
+DB_PASSWORD="Kishor@1234" 
+
+
 
 # Backup WordPress files
 zip -r $BACKUP_DIR/wordpress-files-backup-$TIMESTAMP.zip $WP_PATH
@@ -14,5 +16,5 @@ mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/wordpress-db-backup-
 gzip $BACKUP_DIR/wordpress-db-backup-$TIMESTAMP.sql
 
 # Upload to S3
-aws s3 cp $BACKUP_DIR/wordpress-files-backup-$TIMESTAMP.zip s3://your-s3-bucket-name/backup/
-aws s3 cp $BACKUP_DIR/wordpress-db-backup-$TIMESTAMP.sql.gz s3://your-s3-bucket-name/backup/
+aws s3 cp $BACKUP_DIR/wordpress-files-backup-$TIMESTAMP.zip s3://wp-backup-al/backup/
+aws s3 cp $BACKUP_DIR/wordpress-db-backup-$TIMESTAMP.sql.gz s3://wp-backup-al/backup/
